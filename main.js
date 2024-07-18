fffe64b650182b47a65d61fec6f9328d1b20b445
amyuzu <baske.yuzu98@gmail.com>
2024-07-18 16:02:15 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 $(function(){
 	/*--画像の切り替え--*/
 	//サムネイル画像をクリックした際の処理
-	$('#thumbnail img').on('click', function(){
+	$('#thumbnail li').on('click', function(){
 
-	//ホバーされた画像のパス取得
-	//$(this)はイベントが発生した要素を指す
-	//1つ目のサムネイルがクリックされた際は1つ目のsrc、
-	//2つ目のサムネイルがクリックされた際は2つ目のsrcを取得する
-	let img_src = $(this).attr("src");
+		//ホバーされた画像のパスと見出し、説明を取得
+		//$(this)はイベントが発生した要素を指す
+		//1つ目のサムネイルがクリックされた際は1つ目のsrcとheading,description、
+		//2つ目のサムネイルがクリックされた際は2つ目のsrcとheading,descriptionを取得する
+		let img_src = $(this).data("image");
+		let img_heading = $(this).data("heading");
+		let img_description = $(this).find('.thumbnail-description').text();
 
 		//メイン画像と同じサムネイルがクリックされた際は画像切り替えを実行しない
 		//（メイン画像のsrcとクリックされた画像のsrcが異なる場合のみ実行）
-		if($('mainvisual img').attr("src") != img_src) {
+		if($('#main-image').attr("src") != img_src) {
 
 			//メイン画像を0.5秒かけてフェードアウト
-			//フェードアウト完了後、メイン画像のsrcをクリックされたサムネイルのsrcに変更
-			//メイン画像を0.5秒でフェードイン
-			//何かの処理が完了後に実行させる場合はコールバック関数を使用
-			$('#mainvisual img').fadeOut(500, function() {
-				$("#mainvisual img").attr({src:img_src});
-				$("#mainvisual img").fadeIn(500);
-			})
+			$('#main-image').fadeOut(500, function() {
+				//フェードアウト完了後、メイン画像のsrcをクリックされたサムネイルのsrcに変更
+				$("#main-image").attr("src", img_src);
+				//メイン画像を0.5秒でフェードイン
+				//何かの処理が完了後に実行させる場合はコールバック関数を使用
+				$("#main-image").fadeIn(500);
+			});
+
+			//見出しを変更
+			$('#main-heading').text(img_heading);
+			$('#main-description').text(img_description);
+
+			//説明文表示
+			$('.description').fadeIn(500);
 		}
 	});
 });
